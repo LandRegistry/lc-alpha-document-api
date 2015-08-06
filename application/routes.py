9b2aecc37d0c5d48b2e8d3e0c68f560ddca43230
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-08-06 15:55:43 +0100

    Empty routes added.

--- application/routes.py
+++ application/routes.py
@@ -1,8 +1,54 @@
 from application import app
 from flask import Response, request
 
+# Mock document management system.
+# A document will be some metadata, plus images of each of the pages of the document
+# Images will be stored in whatever format they're sent (in legacy world: TIFF & JPEG)
+
 
 @app.route('/', methods=["GET"])
 def index():
     return Response(status=200)
+
+
+@app.route('/document', methods=["POST"])
+def create_documents():
+    # create an empty document meta-entry
+    pass
+
+
+@app.route('/document/<int:doc_no>', methods=["GET"])
+def get_document(doc_no):
+    # retrieve the meta-entry, including URIs of the images
+    pass
+
+
+@app.route('/document/<int:doc_no>', methods=["PUT"])
+def change_document(doc_no):
+    # set new meta-data for a document
+    pass
+
+
+@app.route('/document/<int:doc_no>', methods=["DELETE"])
+def delete_document(doc_no):
+    # delete an entire document and its associated images
+    pass
+
+
+@app.route('/document/<int:doc_no>/image/<int:index>', methods=["GET"])
+def get_image(doc_no, index):
+    # get an image
+    pass
+
+
+@app.route('/document/<int:doc_no>/image/<int:index>', methods=["PUT"])
+def put_image(doc_no, index):
+    # set or replace an image
+    pass
+
+
+@app.route('/document/<int:doc_no>/image/<int:index>', methods=["DELETE"])
+def delete_image(doc_no, index):
+    # delete an image from the document
+    pass
 
