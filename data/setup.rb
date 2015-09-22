a54923270a64382270c4b22e809bc279dc9fc4e7
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-09-22 11:58:45 +0100

    Test data

---
+++ data/setup.rb
@@ -0,0 +1,13 @@
+
+if ARGV.length > 0
+    folder = ARGV[0]
+else
+    abort('No directory')
+end
+
+['img30_1.jpeg', 'img32_1.jpeg', 'img34_1.jpeg', 'img36_1.jpeg', 'img38_1.jpeg',
+'img31_1.jpeg', 'img33_1.jpeg', 'img35_1.jpeg', 'img37_1.jpeg', 'img39_1.jpeg', 'img40_1.jpeg',
+'img41_1.jpeg', 'img41_2.jpeg', 'img42_1.jpeg', 'img43_1.jpeg', 'img44_1.jpeg', 'img45_1.jpeg',
+'img51_1.jpeg', 'img52_1.jpeg', 'img53_1.jpeg'].each do |image|
+    `cp #{folder}/data/#{image} ~/#{image}`
+end
