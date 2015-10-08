12451c4cf84364120e186149b2782f6229f105a6
Steve Millis <steve.millis@digital.landregistry.gov.uk>
2015-10-08 13:43:19 +0100

    Create forms for usability testing

--- data/setup.rb
+++ data/setup.rb
@@ -8,6 +8,7 @@ end
 ['img30_1.jpeg', 'img32_1.jpeg', 'img34_1.jpeg', 'img36_1.jpeg', 'img38_1.jpeg',
 'img31_1.jpeg', 'img33_1.jpeg', 'img35_1.jpeg', 'img37_1.jpeg', 'img39_1.jpeg', 'img40_1.jpeg',
 'img41_1.jpeg', 'img41_2.jpeg', 'img42_1.jpeg', 'img43_1.jpeg', 'img44_1.jpeg', 'img44_2.jpeg',
-'img45_1.jpeg', 'img45_2.jpeg', 'img51_1.jpeg', 'img52_1.jpeg', 'img53_1.jpeg', 'img54_1.jpeg'].each do |image|
+'img45_1.jpeg', 'img45_2.jpeg', 'img51_1.jpeg', 'img52_1.jpeg', 'img53_1.jpeg', 'img54_1.jpeg',
+'img55_1.jpeg', 'img56_1.jpeg'].each do |image|
     `cp #{folder}/data/#{image} ~/#{image}`
 end
